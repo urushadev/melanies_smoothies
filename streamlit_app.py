33bc7968de42ca705f23a607d1a40eea5d08a0c0
Urusha Khadka <urushadev@gmail.com>
2025-10-15 22:41:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,70 @@
+# #------------- Database----------------------
+
+# # Import python packages
+# import streamlit as st
+# from snowflake.snowpark.functions import col
+
+# # Write directly to the app
+# st.title(f"Customize Your Smoothie:cup_with_straw:")
+# st.write(
+#   """Choose the fruits you want in your custom Smoothie!
+#   """
+# ) 
+
+# name_on_order= st.text_input("Name on Smoothie:")
+# st.write("The name on your smoothie would be: ", name_on_order)
+
+
+# # We brought col here to select from column
+
+# # session = get_active_session()
+# cnx=st.connection("snowflake")
+# session=cnx.session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+# ingredients_list= st.multiselect(
+#     'Choose up to 5 ingredients:'
+#     ,my_dataframe,
+#     max_selections=5
+    
+# )
+
+# import streamlit as st
+
+
+# if ingredients_list:
+
+#  ingredients_string=''
+    
+#  for x in ingredients_list:
+#      ingredients_string += x+ ' '
+
+#  # st.write(ingredients_string)
+#  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+#             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+            
+#  # st.write(my_insert_stmt)
+
+#  time_to_insert=st.button('Submit Order')
+
+#  if time_to_insert:
+#     session.sql(my_insert_stmt).collect()
+#     st.success('Your Smoothie is ordered!', icon="✅")
+
+
+
+
+#------------- API ----------------------
+
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie:cup_with_straw:")
@@ -18,13 +82,13 @@ st.write("The name on your smoothie would be: ", name_on_order)
 # session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
 
 
 ingredients_list= st.multiselect(
     'Choose up to 5 ingredients:'
-    ,my_dataframe,
+    # ,my_dataframe,
     max_selections=5
     
 )
